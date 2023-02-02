5b9b40a1e59dc7a79c4fc05c940d11c5c0c243a4
Imran Fazilov <ifazilov3584@bths.edu>
2023-02-02 12:26:02 -0500

    period end

--- src/ArrayLists2.java
+++ src/ArrayLists2.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class ArrayLists2 {
+import java.util.ArrayList;
+
+public class ArrayLists2
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Cat> catList = new ArrayList<Cat>();
+        catList.add(new Cat("Patty"));
+        catList.add(new Cat("Keith"));
+        catList.add(new Cat("Jerry"));
+        catList.add(new Cat("Mary"));
+
+        // COMPLETE ME: print each Cat’s name using a
+        // standard for loop
+        for (int i = 0; i < catList.size(); i++)
+        {
+            System.out.println(catList.get(i).getName());
+        }
+        // COMPLETE ME: print each Cat’s name using an
+        // enhanced for loop
+        for (Cat cat : catList)
+        {
+            System.out.println(cat.getName());
+        }
+        // COMPLETE ME: print each Cat’s name (a third
+        // time!) using a while loop
+        int i = 0;
+        while (i < catList.size())
+        {
+            System.out.println(catList.get(i).getName());
+            i++;
+        }
+    }
 }
+
--- src/Digits.java
+++ src/Digits.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Digits {
+import java.util.ArrayList;
+
+public class Digits
+{
+    /** The list of digits from the number used to construct this object.
+     *  The digits appear in the list in the same order in which they appear in the original number.
+     */
+    private ArrayList<Integer> digitList;
+
+    /** Constructs a Digits object that represents num.
+     *  Precondition: num >= 0
+     */
+    public Digits(int num)
+    {
+        for (int i = 0; i < num)
+    }
+
+    /** Returns true if the digits in this Digits object are in strictly increasing order;
+     *  false otherwise.
+     */
+    public boolean isStrictlyIncreasing()
+    {
+        /* to be implemented in part (b) */
+    }
+
+    // GETTER METHOD: added to enable test code (not provided/needed in original FRQ)
+    public ArrayList<Integer> getDigitList()
+    {
+        return digitList;
+    }
 }
